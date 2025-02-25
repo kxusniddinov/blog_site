5ceecca3e9361369e75bba11dbd47fbd21f62220
Komronbek <kxusniddinov25@gmail.com>
2025-02-25 22:44:47 +0500

    update models

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -5,7 +5,7 @@ from .views import PostListView
 app_name = "blogapp"
 
 urlpatterns = [
-    path('', PostListView.as_view(), name="post_list"),
+    path('', views.PostListView.as_view(), name="post_list"),
     path("<int:year>/<int:month>/<int:day>/<slug:slug>/",
          views.post_detail,
          name="post_detail")
